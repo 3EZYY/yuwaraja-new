a9796aaca045f9825de892397c7501492d8cfe07
Rafif Nabiha <rafifnabiha24@student.ub.ac.id>
2025-07-14 17:47:59 +0700

    perbaikan di database_seeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,10 +8,10 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Panggil semua seeder di sini
+
         $this->call([
-            Userdata_Seeder::class,
             Kelompok_Seeder::class,
+            Userdata_Seeder::class,
             Pengumuman_Seeder::class,
             Tugas_Seeder::class,
             Jadwal_Acara_Seeder::class,
